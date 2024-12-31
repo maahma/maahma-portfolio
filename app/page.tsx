0565f18873b270a9872d679f6b3165e71ad3664e
maahma <maahaahmad99@gmail.com>
2024-12-31 21:44:31 +0000

    fixing a bug

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           <a href="mailto:maahaahmad99@gmail.com"><Image className="mail" src="images/mail.svg" alt="mail-icon"/></a>
           <a href="https://github.com/maahma" target="_blank"><Image className="github" src="images/github.svg" alt="git-icon"/></a>
           <a href="https://www.linkedin.com/in/maaha-ahmad/" target="_blank"><Image className="linkedin" src="images/linkedin.svg" alt="linkedin-icon" /></a>
-          <a href="docs/MaahaCV.pdf" target="_blank" rel="noopener noreferrer"><Image className="cv-view" src="images/viewcv.svg" alt="cv-icon" /></a>
+          <a href="/docs/MaahaCV.pdf" target="_blank" rel="noopener noreferrer"><Image className="cv-view" src="images/viewcv.svg" alt="cv-icon" /></a>
         </div>
       </div>
     </main>
